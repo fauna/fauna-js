19cd8286bdfdea3f75d0059f11e14292f72c14bd
Cleve Stuart <90649124+cleve-fauna@users.noreply.github.com>
2022-10-19 14:02:15 -0700

    [FE-2782] Determine error types (#7)

--- __tests__/integration/query.test.ts
+++ __tests__/integration/query.test.ts
@@ -1,6 +1,13 @@
 import { Client } from "../../src/client";
 import { endpoints } from "../../src/client-configuration";
-import { ClientError, ServiceError } from "../../src/wire-protocol";
+import {
+  AuthenticationError,
+  ClientError,
+  NetworkError,
+  ProtocolError,
+  QueryCheckError,
+  QueryRuntimeError,
+} from "../../src/wire-protocol";
 import { env } from "process";
 
 const client = new Client({
@@ -17,29 +24,80 @@ describe("query", () => {
     expect(result).toEqual({ data: 4, txn_time: result.txn_time });
   });
 
-  it("throws a QueryError if the query is invalid", async () => {
+  it("throws a QueryCheckError if the query is invalid", async () => {
     expect.assertions(5);
     try {
       await client.query<number>({ query: '"taco".length;' });
     } catch (e) {
-      expect(e instanceof ServiceError).toBe(true);
-      if (e instanceof ServiceError) {
+      if (e instanceof QueryCheckError) {
         expect(e.message).toEqual("The query failed 1 validation check");
         expect(e.code).toEqual("invalid_query");
         expect(e.httpStatus).toEqual(400);
         expect(e.summary).toEqual(
-          'invalid_syntax: Expected ([ \\t\\n\\r] | lineComment | blockComment | end-of-input):1:14, found ";"\n' +
-            "  |\n" +
-            '1 | "taco".length;\n' +
-            '  |              ^ Expected ([ \\t\\n\\r] | lineComment | blockComment | end-of-input):1:14, found ";"\n' +
-            "  |"
+          'invalid_syntax: Expected ([ \\t\\n\\r] | lineComment\
+ | blockComment | end-of-input):1:14, found ";"\n\
+  |\n\
+1 | "taco".length;\n\
+  |              ^ Expected ([ \\t\\n\\r] | lineComment | blockComment | end-of-input):1:14, found ";"\n\
+  |'
+        );
+        expect(e.failures).toEqual([
+          {
+            code: "invalid_syntax",
+            message:
+              'Expected ([ \\t\\n\\r] | lineComment | blockComment | end-of-input):1:14, found ";"',
+          },
+        ]);
+      }
+    }
+  });
+
+  it("throws a QueryRuntimeError if the query hits a runtime error", async () => {
+    expect.assertions(3);
+    try {
+      await client.query({ query: '"taco".length + "taco"' });
+    } catch (e) {
+      if (e instanceof QueryRuntimeError) {
+        expect(e.httpStatus).toEqual(400);
+        expect(e.code).toEqual("invalid_argument");
+        expect(e.summary).toEqual(
+          "invalid_argument: expected value for `other` of type number, received string\n" +
+            "0: *query*:1\n" +
+            "    |\n" +
+            '  1 | "taco".length + "taco"\n' +
+            "    | ^^^^^^^^^^^^^^^^^^^^^^\n" +
+            "    |"
         );
       }
     }
   });
 
-  it("throws a ClientError if the client fails.", async () => {
-    expect.assertions(3);
+  it("throws a AuthenticationError creds are invalid", async () => {
+    expect.assertions(4);
+    const badClient = new Client({
+      endpoint: env["endpoint"] ? new URL(env["endpoint"]) : endpoints.local,
+      maxConns: 5,
+      secret: "nah",
+      queryTimeoutMillis: 60,
+    });
+    try {
+      await badClient.query<number>({ query: '"taco".length' });
+    } catch (e) {
+      if (e instanceof AuthenticationError) {
+        expect(e.message).toEqual("Unauthorized: Access token required");
+        expect(e.code).toEqual("unauthorized");
+        expect(e.httpStatus).toEqual(401);
+        expect(e.summary).toBeUndefined();
+      }
+    }
+  });
+
+  it("throws a AuthorizationError if creds are not permissioned.", async () => {
+    // TODO - AuthZ checks are not yet implemented.
+  });
+
+  it("throws a NetworkError if the connection fails.", async () => {
+    expect.assertions(2);
     const myBadClient = new Client({
       endpoint: new URL("http://localhost:1"),
       maxConns: 1,
@@ -49,12 +107,52 @@ describe("query", () => {
     try {
       await myBadClient.query<number>({ query: '"taco".length;' });
     } catch (e) {
-      expect(e instanceof ClientError).toBe(true);
+      if (e instanceof NetworkError) {
+        expect(e.message).toEqual(
+          "The network connection encountered a problem."
+        );
+        expect(e.cause).not.toBeUndefined();
+      }
+    }
+  });
+
+  it("throws a ClientError if the client fails unexpectedly", async () => {
+    expect.assertions(2);
+    const myBadClient = new Client({
+      endpoint: env["endpoint"] ? new URL(env["endpoint"]) : endpoints.local,
+      maxConns: 5,
+      secret: env["secret"] || "secret",
+      queryTimeoutMillis: 60,
+    });
+    myBadClient.client.post = () => {
+      throw new Error("boom!");
+    };
+    try {
+      await myBadClient.query({ query: "foo" });
+    } catch (e) {
       if (e instanceof ClientError) {
+        expect(e.cause).not.toBeUndefined();
         expect(e.message).toEqual(
           "A client level error occurred. Fauna was not called."
         );
-        expect(e.cause).not.toBeUndefined();
+      }
+    }
+  });
+
+  it("throws a ProtocolError if the http fails outside Fauna", async () => {
+    expect.assertions(2);
+    const badClient = new Client({
+      endpoint: new URL("https://frontdoor.fauna.com/"),
+      maxConns: 5,
+      secret: "nah",
+      queryTimeoutMillis: 60,
+    });
+    try {
+      await badClient.query({ query: "foo" });
+    } catch (e) {
+      if (e instanceof ProtocolError) {
+        expect(e.httpStatus).toBeGreaterThanOrEqual(400);
+        expect(e.message).not.toBeUndefined();
       }
     }
   });
--- src/client.ts
+++ src/client.ts
@@ -1,12 +1,43 @@
-import axios, { type Axios } from "axios";
+import axios, { AxiosInstance } from "axios";
 import type { ClientConfiguration } from "./client-configuration";
 import Agent, { HttpsAgent } from "agentkeepalive";
 import {
+  AuthenticationError,
+  AuthorizationError,
   ClientError,
+  NetworkError,
+  ProtocolError,
+  QueryCheckError,
+  QueryCheckFailure,
+  QueryRuntimeError,
+  QueryTimeoutError,
   ServiceError,
+  ServiceInternalError,
+  ServiceTimeoutError,
+  Span,
+  ThrottlingError,
   type QueryRequest,
   type QueryResponse,
 } from "./wire-protocol";
+
+const nodeOrAxiosNetworkErrorCodes = [
+  "ECONNABORTED",
+  "ECONNREFUSED",
+  "ECONNRESET",
+  "ERR_NETWORK",
+  "ETIMEDOUT",
+  // axios does not yet support http2, but preparing
+  // in case we move to a library that does or axios
+  // adds in support.
+  "ERR_HTTP_REQUEST_TIMEOUT",
+  "ERR_HTTP2_GOAWAY_SESSION",
+  "ERR_HTTP2_INVALID_SESSION",
+  "ERR_HTTP2_INVALID_STREAM",
+  "ERR_HTTP2_OUT_OF_STREAMS",
+  "ERR_HTTP2_SESSION_ERROR",
+  "ERR_HTTP2_STREAM_CANCEL",
+  "ERR_HTTP2_STREAM_ERROR",
+];
 
 /**
  * Client for calling Fauna.
@@ -14,8 +45,8 @@ import {
 export class Client {
   /** The {@link ClientConfiguration} */
   readonly clientConfiguration: ClientConfiguration;
-  /** The underlying {@link Axios} client. */
-  readonly client: Axios;
+  /** The underlying {@link AxiosInstance} client. */
+  readonly client: AxiosInstance;
 
   /**
    * Constructs a new {@link Client}.
@@ -63,8 +94,18 @@ export class Client {
   /**
    * Queries Fauna.
    * @param queryRequest - the {@link QueryRequest}
-   * @returns A {@link QueryResponse}.
-   * @throws A {@link QueryError} if an error is returned by Fauna.
+   * @returns {@link QueryResponse}.
+   * @throws {@link ServiceError} Fauna emitted an error. The ServiceError will be
+   *   one of ServiceError's child classes if the error can be further categorized,
+   *   or a concrete ServiceError if it cannot. ServiceError child types are
+   *   {@link AuthenticaionError}, {@link AuthorizationError}, {@link QueryCheckError}
+   *   {@link QueryRuntimeError}, {@link QueryTimeoutError}, {@link ServiceInternalError}
+   *   {@link ServiceTimeoutError}, {@link ThrottlingError}.
+   *   You can use either the type, or the underlying httpStatus + code to determine
+   *   the root cause.
+   * @throws {@link ProtocolError} the client a HTTP error not sent by Fauna.
+   * @throws {@link NetworkError} the client encountered a network issue
+   * connecting to Fauna.
    * @throws A {@link ClientError} the client fails to submit the request
    * due to an internal error.
    */
@@ -78,11 +119,33 @@ export class Client {
     } catch (e: any) {
       // see: https://axios-http.com/docs/handling_errors
       if (e.response) {
-        throw new ServiceError({
-          ...(e.response?.data?.error || { message: e.message }),
+        // we got an error from the fauna service
+        if (e.response.data?.error) {
+          throw this.#getServiceError(e.response.data.error, e.response.status);
+        }
+        // we got a different error from the protocol layer
+        throw new ProtocolError({
+          message: e.message,
           httpStatus: e.response.status,
         });
       }
+      // we're in the browser dealing with an XMLHttpRequest that was never sent
+      // OR we're in node dealing with an HTTPClient.Request that never connected
+      // OR node or axios hit a network connection problem at a lower level,
+      // OR axios threw a network error
+      // see: https://nodejs.org/api/errors.html#nodejs-error-codes
+      if (
+        e.request?.status === 0 ||
+        e.request?.socket?.connecting ||
+        nodeOrAxiosNetworkErrorCodes.includes(e.code) ||
+        "Network Error" === e.message
+      ) {
+        throw new NetworkError(
+          "The network connection encountered a problem.",
+          { cause: e }
+        );
+      }
+      // unknown error
       throw new ClientError(
         "A client level error occurred. Fauna was not called.",
         {
@@ -90,5 +153,52 @@ export class Client {
         }
       );
     }
+  }
+
+  #getServiceError(
+    error: {
+      code: string;
+      message: string;
+      summary?: string;
+      failures?: Array<QueryCheckFailure>;
+      stats?: { [key: string]: number };
+      trace?: Array<Span>;
+      txn_time?: string;
+    },
+    httpStatus: number
+  ): ServiceError {
+    if (httpStatus === 401) {
+      return new AuthenticationError({ httpStatus, ...error });
+    }
+    if (httpStatus === 403) {
+      return new AuthorizationError({ httpStatus, ...error });
+    }
+    if (httpStatus === 500) {
+      return new ServiceInternalError({ httpStatus, ...error });
+    }
+    if (httpStatus === 503) {
+      return new ServiceTimeoutError({ httpStatus, ...error });
+    }
+    if (httpStatus === 429) {
+      return new ThrottlingError({ httpStatus, ...error });
+    }
+    if (httpStatus === 440) {
+      // TODO stats not yet returned. Include it when it is.
+      return new QueryTimeoutError({ httpStatus, ...error });
+    }
+    // TODO trace, txn_time, and stats not yet returned for QueryRuntimeError
+    // flip to check for those rather than a specific code.
+    if (httpStatus === 400 && error.code === "invalid_argument") {
+      return new QueryRuntimeError({ httpStatus, ...error });
+    }
+    if (httpStatus === 400 && error.failures !== undefined) {
+      // same trick
+      return new QueryCheckError({
+        httpStatus,
+        ...error,
+        failures: error.failures,
+      });
+    }
+    return new ServiceError({ httpStatus, ...error });
   }
 }

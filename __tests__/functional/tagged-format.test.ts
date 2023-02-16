9d7127e07be3e05282c10b99d5ad3019bb647ebd
Darren Cunningham <darren.cunningham@fauna.com>
2023-02-16 06:22:30 -0500

    basic implementation

---
+++ __tests__/functional/tagged-format.test.ts
@@ -0,0 +1,114 @@
+import {
+  TaggedTypeFormat,
+  DocumentReference,
+  Module,
+} from "../../src/tagged-type";
+
+describe("tagged format", () => {
+  it("can be decoded", () => {
+    const allTypes: string = `{
+      "bugs_coll": { "@mod": "Bugs" },
+      "bug": { "@doc": "Bugs:123" },
+      "name": "fir",
+      "age": { "@int": "200" },
+      "birthdate": { "@date": "1823-02-08" },
+      "molecules": { "@long": "999999999999999999" },
+      "circumference": { "@double": "3.82" },
+      "created_at": { "@time": "2003-02-08T13:28:12.000555+00:00" },
+      "extras": {
+        "nest": {
+          "@object": {
+            "num_sticks": { "@int": "58" },
+            "@extras": { "egg": { "fertilized": false } }
+          }
+        }
+      },
+      "measurements": [
+        {
+          "id": { "@int": "1" },
+          "employee": { "@int": "3" },
+          "time": { "@time": "2013-02-08T12:00:05.000123+00:00" }
+        },
+        {
+          "id": { "@int": "2" },
+          "employee": { "@int": "5" },
+          "time": { "@time": "2023-02-08T14:22:01.000001+00:00" }
+        }
+      ]
+    }`;
+
+    const bugs_mod: Module = "Bugs";
+    const bugs_doc: DocumentReference = "Bugs:123";
+
+    const result = TaggedTypeFormat.decode(allTypes);
+    expect(result.bugs_coll).toBe(bugs_mod);
+    expect(result.bug).toBe(bugs_doc);
+    expect(result.name).toEqual("fir");
+    expect(result.age).toEqual(200);
+    expect(result.birthdate).toBeInstanceOf(Date);
+    expect(result.molecules).toEqual(999999999999999999);
+    expect(result.circumference).toEqual(3.82);
+    expect(result.created_at).toBeInstanceOf(Date);
+    expect(result.extras.nest.num_sticks).toEqual(58);
+    expect(result.extras.nest["@extras"].egg.fertilized).toBe(false);
+    expect(result.measurements).toHaveLength(2);
+    expect(result.measurements[0].id).toEqual(1);
+    expect(result.measurements[0].employee).toEqual(3);
+    expect(result.measurements[0].time).toBeInstanceOf(Date);
+    expect(result.measurements[1].id).toEqual(2);
+    expect(result.measurements[1].employee).toEqual(5);
+    expect(result.measurements[1].time).toBeInstanceOf(Date);
+  });
+
+  it("can be encoded", () => {
+    let result = JSON.stringify(
+      TaggedTypeFormat.encode({
+        child: { more: { itsworking: new Date("1983-04-15") } },
+        date: new Date("1923-05-13"),
+        decimal: 4.14,
+        long: 32,
+        name: "Hello, World",
+        number: 48,
+        time: new Date("2023-01-30T16:27:45.204243-05:00"),
+        extra: [
+          {
+            id: 1,
+            time: new Date(),
+          },
+          {
+            id: 2,
+            time: new Date(),
+          },
+        ],
+        "@foobar": {
+          date: new Date("1888-08-08"),
+        },
+      })
+    );
+
+    // expect(result).toEqual(
+    //   `{"child":{"more":{"itsworking":{"@date":"1983-04-15T00:00:00.000Z"}}},"date":{"@date":"1923-05-13T00:00:00.000Z"},"decimal":{"@decimal":4.14},"long":{"@int":32},"name":"Hello, World","number":{"@int":48},"time":{"@time":"2023-01-30T21:27:45.204Z"}}`
+    // );
+    const backToObj = JSON.parse(result);
+    expect(backToObj.decimal).toStrictEqual({ "@decimal": 4.14 });
+    expect(backToObj.extra).toHaveLength(2);
+    expect(backToObj.child.more.itsworking).toStrictEqual({
+      "@date": "1983-04-15T00:00:00.000Z",
+    });
+  });
+
+  it("handleds conflicts", () => {
+    var result = TaggedTypeFormat.encode({
+      "@date": new Date(2022, 11, 1),
+      "@time": new Date(2022, 11, 2),
+      "@int": 1,
+      "@long": 9999999999999,
+      "@double": 1.99,
+    });
+    expect(result["@date"]).toEqual(new Date(2022, 11, 1));
+    expect(result["@time"]).toEqual(new Date(2022, 11, 2));
+    expect(result["@int"]).toEqual(1);
+    expect(result["@long"]).toEqual(9999999999999);
+    expect(result["@double"]).toEqual(1.99);
+  });
+});
---
+++ src/tagged-type.ts
@@ -0,0 +1,117 @@
+export type Module = string;
+export type DocumentReference = string;
+
+/**
+ * TaggedType provides the encoding/decoding of the Fauna Tagged Type formatting
+ */
+export class TaggedTypeFormat {
+  /**
+   * Encode the Object to the Tagged Type format for Fauna
+   *
+   * @param obj - Object that will be encoded
+   * @returns Map of result
+   */
+  static encode(obj: any): any {
+    return new taggedTypeEncoded(obj).result;
+  }
+
+  /**
+   * Decode the JSON string result from Fauna to remove Tagged Type formatting.
+   *
+   * @param input - JSON string result from Fauna
+   * @returns object of result of FQL query
+   */
+  static decode(input: string): any {
+    return JSON.parse(input, (_, value: any) => {
+      if (value["@mod"]) {
+        return value["@mod"] as Module;
+      } else if (value["@doc"]) {
+        return value["@doc"] as DocumentReference;
+      } else if (value["@int"]) {
+        return Number(value["@int"]);
+      } else if (value["@long"]) {
+        return Number(value["@long"]);
+      } else if (value["@decimal"]) {
+        return Number(value["@decimal"]);
+      } else if (value["@double"]) {
+        return Number(value["@double"]);
+      } else if (value["@date"]) {
+        return new Date(value["@date"]);
+      } else if (value["@time"]) {
+        return new Date(value["@time"]);
+      } else if (value["@object"]) {
+        return value["@object"];
+      }
+
+      return value;
+    });
+  }
+}
+
+class taggedTypeEncoded {
+  readonly result: any;
+
+  readonly #encodeMap = {
+    number: (value: number): { [key: string]: number } => {
+      if (`${value}`.indexOf(".") > 0) {
+        return { "@decimal": value };
+      } else {
+        return { "@int": value };
+      }
+    },
+    string: (value: string): string => {
+      return value;
+    },
+    object: (input: any): { [key: string]: string } => {
+      const _out: { [key: string]: any } = {};
+      for (const k in input) {
+        if (k.startsWith("@")) {
+          _out[k] = input[k];
+        } else {
+          _out[k] = TaggedTypeFormat.encode(input[k]);
+        }
+      }
+      return _out;
+    },
+    array: (input: Array<any>): Array<any> => {
+      const _out: any = [];
+      for (const i in input) _out.push(TaggedTypeFormat.encode(input[i]));
+
+      return _out;
+    },
+    date: (dateValue: Date): { [key: string]: string } => {
+      if (
+        dateValue.getUTCHours() == 0 &&
+        dateValue.getUTCMinutes() == 0 &&
+        dateValue.getUTCSeconds() == 0 &&
+        dateValue.getUTCMilliseconds() == 0
+      ) {
+        return { "@date": dateValue.toISOString() };
+      }
+
+      return { "@time": dateValue.toISOString() };
+    },
+  };
+
+  constructor(input: any) {
+    this.result = {};
+
+    switch (typeof input) {
+      case "string":
+        this.result = this.#encodeMap["string"](input);
+        break;
+      case "number":
+        this.result = this.#encodeMap["number"](input);
+        break;
+      case "object":
+        if (Array.isArray(input)) {
+          this.result = this.#encodeMap["array"](input);
+        } else if (input instanceof Date) {
+          this.result = this.#encodeMap["date"](input);
+        } else {
+          this.result = this.#encodeMap["object"](input);
+        }
+        break;
+    }
+  }
+}

fc122177a64177074255fb09a1fd25cd1d5e12bf
Paul Paterson <ptpaterson@gmail.com>
2023-03-02 14:36:15 -0500

    Ignore the lib folder (#79)

--- lib/index.d.ts
+++
@@ -1,5 +0,0 @@
-export { Client } from "./client";
-export { type ClientConfiguration, type Endpoints, endpoints, } from "./client-configuration";
-export { AuthenticationError, AuthorizationError, ClientError, NetworkError, ProtocolError, QueryCheckError, QueryRuntimeError, QueryTimeoutError, ServiceError, ServiceInternalError, ServiceTimeoutError, ThrottlingError, } from "./errors";
-export { type QueryBuilder, fql } from "./query-builder";
-export { type JSONObject, type JSONValue, type QueryFailure, type QueryInfo, type QueryInterpolation, type QueryRequest, type QueryRequestHeaders, type QueryStats, type QuerySuccess, type Span, type ValueFragment, } from "./wire-protocol";

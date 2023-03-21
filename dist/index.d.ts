3adbf385e42026548c8b50341ec8baa6fea3b019
Cleve Stuart <cleve.stuart@fauna.com>
2023-03-21 10:51:51 -0700

    Export values

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -3,3 +3,4 @@ export { type ClientConfiguration, type Endpoints, endpoints, } from "./client-c
 export { AuthenticationError, AuthorizationError, ClientError, NetworkError, ProtocolError, QueryCheckError, QueryRuntimeError, QueryTimeoutError, ServiceError, ServiceInternalError, ServiceTimeoutError, ThrottlingError, } from "./errors";
 export { type QueryBuilder, fql } from "./query-builder";
 export { type JSONObject, type JSONValue, type QueryFailure, type QueryInfo, type QueryInterpolation, type QueryRequest, type QueryRequestHeaders, type QueryStats, type QuerySuccess, type Span, type ValueFragment, } from "./wire-protocol";
+export { DateStub, Document, DocumentReference, type DocumentT, Module, NamedDocument, NamedDocumentReference, Set, TimeStub } from "./values";

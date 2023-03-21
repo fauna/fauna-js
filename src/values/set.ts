d2be5a9e4287e05171ea5e8d14ebe67eaddff67d
Paul Paterson <ptpaterson@gmail.com>
2023-03-20 20:41:27 -0400

    Hook up domain type encoding/decoding

--- src/values/set.ts
+++ src/values/set.ts
@@ -1,10 +1,10 @@
-import { JSONObject } from "../wire-protocol";
+import { JSONValue } from "../wire-protocol";
 
-export class Set<T extends JSONObject> {
-  readonly data: T;
+export class Set<T extends JSONValue> {
+  readonly data: T[];
   readonly after?: string;
 
-  constructor({ data, after }: { data: T; after?: string }) {
+  constructor({ data, after }: { data: T[]; after?: string }) {
     this.data = data;
     this.after = after;
   }

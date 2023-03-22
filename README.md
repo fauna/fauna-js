fb049a37cd244c57ac85d711f8195c766892d008
Paul Paterson <ptpaterson@gmail.com>
2023-03-22 11:07:20 -0400

    Add Concourse and prepare to publish (#100)
    Co-authored-by: Darren Cunningham <darren.cunningham@fauna.com>

--- README.md
+++ README.md
@@ -1,9 +1,6 @@
 > **Note**
 > This driver is in beta release and not recommended for production use. It operates with the Fauna database service via an API which is also in beta release, and is not recommended for production use. This driver is not compatible with v4 or earlier versions of Fauna. If you would like to participate in the private beta program please contact product@fauna.com.
 
-
-
-
 ## Table of Contents
 
 1. [Development](#development)
---
+++ concourse/scripts/publish.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+
+set -eou
+
+cd ./fauna-js-repository
+
+PACKAGE_VERSION=$(node -p -e "require('./package.json').version")
+NPM_LATEST_VERSION=$(npm view fauna version)
+echo "Current package version: $PACKAGE_VERSION"
+echo "Latest version in npm: $NPM_LATEST_VERSION"
+
+if [ "$PACKAGE_VERSION" \> "$NPM_LATEST_VERSION" ]
+then
+  npm install
+  npm run build
+
+  echo "Publishing a new version..."
+  echo "//registry.npmjs.org/:_authToken=$NPM_TOKEN" > .npmrc
+  npm publish
+  rm .npmrc
+
+  echo "fauna-js@$PACKAGE_VERSION published to npm" > ../slack-message/publish
+else
+  echo "fauna-js@${NPM_LATEST_VERSION} package has been already published" > ../slack-message/publish
+  echo "fauna-js@${NPM_LATEST_VERSION} package has been already published" 1>&2
+  exit 1
+fi

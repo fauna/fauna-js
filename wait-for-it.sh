f4bb21023265db84543d600513fa89c41790f8ff
Cleve Stuart <90649124+cleve-fauna@users.noreply.github.com>
2023-03-23 11:40:44 -0700

    Update health check on integration tests in pipeline to use curl. (#104)

---
+++ wait-for-it.sh
@@ -0,0 +1,14 @@
+attempt_counter=0
+max_attempts=100
+
+until $(curl -m 1 --output /dev/null --silent --head --fail $1); do
+  if [ ${attempt_counter} -eq ${max_attempts} ];then
+    echo ""
+    echo "Max attempts reached to $1"
+    exit 1
+  fi
+
+  printf '.'
+  attempt_counter=$(($attempt_counter+1))
+  sleep 5
+done

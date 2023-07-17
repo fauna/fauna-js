0d39ae6cbec2794530931518fee4d37215f1e92c
fauna-chase <73842483+fauna-chase@users.noreply.github.com>
2023-07-17 16:36:15 -0500

    add schema version to drivers (#174)

---
+++ __tests__/integration/schema-version.test.ts
@@ -0,0 +1,19 @@
+import { fql } from "../../src";
+import { getClient } from "../client";
+
+describe("schema version is returned by the client", () => {
+  const client = getClient();
+  it("returns the schema version", async () => {
+    const resTs = await client.query(fql`
+      Collection.create({ name: "TestColl" })
+    `);
+
+    const expectedSchemaVersion = resTs.txn_ts;
+
+    const res = await client.query(fql`
+      Customers.all()
+    `);
+
+    expect(res.schema_version).toEqual(expectedSchemaVersion);
+  });
+});

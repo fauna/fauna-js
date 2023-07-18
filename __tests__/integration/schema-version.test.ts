fcc2d709decd0d2ff629e7f9be0943b43fe52ead
fauna-chase <73842483+fauna-chase@users.noreply.github.com>
2023-07-18 12:59:54 -0500

    fix schema version test (#182)

--- __tests__/integration/schema-version.test.ts
+++ __tests__/integration/schema-version.test.ts
@@ -5,13 +5,17 @@ describe("schema version is returned by the client", () => {
   const client = getClient();
   it("returns the schema version", async () => {
     const resTs = await client.query(fql`
-      Collection.create({ name: "TestColl" })
+      if (Collection.byName("TestColl") == null) {
+        Collection.create({ name: "TestColl" })
+      } else {
+        TestColl.definition.update({})
+      }
     `);
 
     const expectedSchemaVersion = resTs.txn_ts;
 
     const res = await client.query(fql`
-      Customers.all()
+      TestColl.all()
     `);
 
     expect(res.schema_version).toEqual(expectedSchemaVersion);
